3f096a88439ac31a8217a1df75e1057fcab1b11b
Bruno Biasetti <biasetti@ciandt.com>
2016-03-04 11:43:26 -0300

    JPA poc

--- src/main/java/br/com/appday/product/service/CustomerService.java
+++ src/main/java/br/com/appday/product/service/CustomerService.java
@@ -6,7 +6,6 @@ import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
